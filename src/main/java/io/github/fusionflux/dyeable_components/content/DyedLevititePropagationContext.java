556df88c0944c56c3fcb145c79992d8a651435ea
TropheusJ <jverrellijr@icloud.com>
2026-04-08 19:07:54 -0400

    simplify DyedLevititePropagationContext

--- src/main/java/io/github/fusionflux/dyeable_components/content/DyedLevititePropagationContext.java
+++ src/main/java/io/github/fusionflux/dyeable_components/content/DyedLevititePropagationContext.java
@@ -2,51 +2,18 @@ package io.github.fusionflux.dyeable_components.content;
 
 
 import dev.eriksonn.aeronautics.api.levitite_blend_crystallization.CrystalPropagationContext;
-import dev.eriksonn.aeronautics.api.levitite_blend_crystallization.LevititeBlendHelper;
-import dev.eriksonn.aeronautics.index.*;
+import dev.eriksonn.aeronautics.content.blocks.levitite.LevititeCrystalPropagationContext;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.FluidState;
 import net.neoforged.neoforge.registries.DeferredBlock;
 
-public class DyedLevititePropagationContext implements CrystalPropagationContext {
+public class DyedLevititePropagationContext extends LevititeCrystalPropagationContext {
     private final DeferredBlock<Block> block;
 
     public DyedLevititePropagationContext(DeferredBlock<Block> block) {
         this.block = block;
-    }
-
-    @Override
-    public void onCrystallizationInitialize(Level level, BlockPos pos, boolean isDormant) {
-        level.playSound(null, pos, SoundEvents.FIRE_EXTINGUISH, SoundSource.BLOCKS, 0.2F, 1.5F);
-
-        if (!isDormant) {
-            LevititeBlendHelper.spawnParticles(level, pos, ParticleTypes.FLAME, 20);
-        }
-
-        LevititeBlendHelper.spawnParticles(level, pos, ParticleTypes.SMOKE, 15);
-    }
-
-    @Override
-    public void onCrystallize(Level level, BlockPos pos) {
-        this.onDefaultCrystallize(level, pos);
-
-        if (!level.isClientSide) {
-            AeroSoundEvents.LEVITITE_BLEND_CRYSTALLIZE.play(level, null, pos, 1.0F, 1.0F);
-            LevititeBlendHelper.spawnParticles(level, pos, ParticleTypes.FLAME, 30);
-            AeroAdvancements.UNIDENTIFIED_FLOATING_OBJECT.awardToNearby(pos, level);
-        }
-    }
-
-    @Override
-    public void onCrystallizationFail(Level level, BlockPos pos, int attempts, boolean isDormant) {
-        LevititeBlendHelper.spawnParticles(level, pos, ParticleTypes.SMOKE, 15);
     }
 
     @Override
@@ -55,17 +22,7 @@ public class DyedLevititePropagationContext implements CrystalPropagationContext
     }
 
     @Override
-    public boolean canSpreadTo(FluidState state) {
-        return state.is(LevititeBlendHelper.getFluid());
-    }
-
-    @Override
     public CrystalPropagationContext getContextForSpread(Level level, BlockPos pos) {
         return this;
-    }
-
-    @Override
-    public TagKey<Block> getCatalyzerTag() {
-        return AeroTags.BlockTags.LEVITITE_CATALYZER;
     }
 }
